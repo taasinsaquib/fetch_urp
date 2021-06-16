fafe06b4112d0732fedd00e767ca8905b6c9bca2
Taasin <taasin.saquib@gmail.com>
2021-06-16 00:16:01 -0700

    experiment with retina NN

--- Assets/Scripts/Classification.cs
+++ Assets/Scripts/Classification.cs
@@ -10,25 +10,32 @@ public class Classification : MonoBehaviour
 {
 
 	public NNModel modelAsset;
-	private Model model;
+	// private Model model;
 	IWorker worker;
 	public Text uiText;
 
 	public Dog dog;
 
+	float[][] onv;
+
 	void Start() 
 	{
-        model = ModelLoader.Load(modelAsset);
+        var model = ModelLoader.Load(modelAsset);
         worker = WorkerFactory.CreateWorker(WorkerFactory.Type.ComputePrecompiled, model);
         uiText = GetComponent<Text>(); 
         
+		onv = new float[2][];
+        onv[0] = new float[11880];
+        onv[1] = new float[11880];
 	}
 
 	void Update()
 	 {
 		// Tensor input = new Tensor (1, 1, 2, 11880);
 
-		var onv = dog.getONV();
+		onv = dog.getONV();
+
+
 
 		Tensor input = new Tensor(new TensorShape(1, 1, 2, 11880), onv);
 
